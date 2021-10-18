06e3c8d25214c737257ab5316c2d67a4f91f5d0a
sogafish <kent.soga@gmail.com>
2021-10-18 15:49:42 +0900

    Fix Paths

--- src/constants/screens.ts
+++ src/constants/screens.ts
@@ -8,3 +8,21 @@ export const SCREENS = {
   NOTIFICATION: 'Notification',
   SETTING: 'Setting',
 };
+
+const WEB_PATHS = {
+  FIRST: 'first',
+  SECOND: 'second',
+  HOME: 'home',
+  MAIN: 'main',
+  MY_PAGE: 'my-page',
+  SETTING: 'setting',
+};
+
+export const SCREEN_PATH_MAPS = {
+  [SCREENS.FIRST]: WEB_PATHS.FIRST,
+  [SCREENS.SECOND]: WEB_PATHS.SECOND,
+  [SCREENS.HOME]: WEB_PATHS.HOME,
+  [SCREENS.MAIN]: WEB_PATHS.MAIN,
+  [SCREENS.MY_PAGE]: WEB_PATHS.MY_PAGE,
+  [SCREENS.SETTING]: WEB_PATHS.SETTING,
+};
--- src/navigator/index.tsx
+++ src/navigator/index.tsx
@@ -1,18 +1,11 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { SCREENS } from '@constants/screens';
+import { SCREEN_PATH_MAPS } from '@constants/screens';
 import { Tabs } from './Tabs';
 import { LoadingView } from '../components';
 
 const config = {
-  screens: {
-    [SCREENS.FIRST]: 'first',
-    [SCREENS.SECOND]: 'second',
-    [SCREENS.HOME]: 'home',
-    [SCREENS.MAIN]: 'main',
-    [SCREENS.MY_PAGE]: 'my-page',
-    [SCREENS.SETTING]: 'setting',
-  },
+  screens: SCREEN_PATH_MAPS,
 };
 
 const linking = {
